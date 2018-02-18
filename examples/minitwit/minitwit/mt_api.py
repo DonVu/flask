e87628b7b97c8fed2116b6f3c0341ba5857ea80e
Don Vu <don.vu1212@gmail.com>
2018-02-18 15:39:40 -0800

    Create populatedb stub

--- examples/minitwit/minitwit/mt_api.py
+++ examples/minitwit/minitwit/mt_api.py
@@ -1,7 +1,11 @@
-'" 
-  This is the minitwit web api
-
-"'
+# This is the minitwit web api
 
 import click
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.cli.command('populatedb')
+def populatedb_command():
+    #Populates the database
+    print('Populated the database.');	
