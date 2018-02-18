4b1fa0c42be08b6c0e202a7bd66c041feeec2b84
Don Vu <don.vu1212@gmail.com>
2018-02-17 23:08:46 -0800

    Add insert commands

---
+++ examples/minitwit/minitwit/population.sql
@@ -0,0 +1,18 @@
+INSERT INTO user
+VALUES ('Mickey_Mouse', 'mmouse@aol.com', 'password1');
+
+INSERT INTO user
+VALUES ('Minnie_Mouse', 'minmouse@aol.com', 'password2');
+
+INSERT INTO user
+VALUES ('Donald_Duck', 'dduck@aol.com', 'password3');
+
+
+
+INSERT INTO follower
+VALUES (1, 2);
+
+
+
+INSERT INTO message
+VALUES (1, 'Hey everyone!', 100);	
