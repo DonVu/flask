907d28d5736ad6a65eb3e5e3f468070ede061fc2
Don Vu <don.vu1212@gmail.com>
2018-02-18 21:57:19 -0800

    Implement populationdb flask command

--- examples/minitwit/minitwit/mt_api.py
+++ examples/minitwit/minitwit/mt_api.py
@@ -2,10 +2,20 @@
 
 import click
 from flask import Flask
+from sqlite3 import dbapi2 as sqlite3
+from minitwit import *
 
 app = Flask(__name__)
+
+def populate_db():
+    # Populates the database
+    db = get_db()
+    with app.open_resource('population.sql', mode = 'r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 
 @app.cli.command('populatedb')
 def populatedb_command():
     #Populates the database
-    print('Populated the database.');	
+    populate_db()
+    print('Populated the database.')
--- examples/minitwit/minitwit/population.sql
+++ examples/minitwit/minitwit/population.sql
@@ -1,10 +1,10 @@
-INSERT INTO user
+INSERT INTO user (username, email, pw_hash)
 VALUES ('Mickey_Mouse', 'mmouse@aol.com', 'password1');
 
-INSERT INTO user
+INSERT INTO user (username, email, pw_hash)
 VALUES ('Minnie_Mouse', 'minmouse@aol.com', 'password2');
 
-INSERT INTO user
+INSERT INTO user (username, email, pw_hash)
 VALUES ('Donald_Duck', 'dduck@aol.com', 'password3');
 
 
@@ -14,5 +14,5 @@ VALUES (1, 2);
 
 
 
-INSERT INTO message
+INSERT INTO message (author_id, text, pub_date)
 VALUES (1, 'Hey everyone!', 100);	
