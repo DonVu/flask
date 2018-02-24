02c6887afa41b9478ce4e65c8c9e92c388d80b04
Don Vu <don.vu1212@gmail.com>
2018-02-24 12:12:38 -0800

    Add entries to population.sql

--- examples/minitwit/minitwit/population.sql
+++ examples/minitwit/minitwit/population.sql
@@ -7,12 +7,39 @@ VALUES ('Minnie_Mouse', 'minmouse@aol.com', 'password2');
 INSERT INTO user (username, email, pw_hash)
 VALUES ('Donald_Duck', 'dduck@aol.com', 'password3');
 
+INSERT INTO user (username, email, pw_hash)
+VALUES ('Goofy', 'goofy@aol.com', 'password4');
 
+INSERT INTO user (username, email, pw_hash)
+VALUES ('Daisy', 'daisy@aol.com', 'password5');
+
+INSERT INTO user (username, email, pw_hash)
+VALUES ('Pluto', 'pluto@aol.com', 'password6');
 
 INSERT INTO follower
 VALUES (1, 2);
 
+INSERT INTO follower
+VALUES (1, 3);
 
+INSERT INTO follower
+VALUES (2, 3);
+
+INSERT INTO follower
+VALUES (4, 3);
+
+INSERT INTO follower
+VALUES (5, 1);
+
+INSERT INTO follower
+VALUES (3, 2);
 
 INSERT INTO message (author_id, text, pub_date)
-VALUES (1, 'Hey everyone!', 100);	
+VALUES (1, 'Hey everyone!', 100); 
+
+INSERT INTO message (author_id, text, pub_date)
+VALUES (2, 'Hey! Goodmorning!', 100);
+
+INSERT INTO message (author_id, text, pub_date)
+VALUES (5, 'Have a great day guys!', 100); 
+
